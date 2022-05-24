0c133d82c84f12f719387daa83a47dd0992fee49
Flavio Mauricio <flavio.m.valerio@accenture.com>
2022-05-24 12:03:16 -0300

    refactor: refactor display-imagem

--- src/components/Foto/index.jsx
+++ src/components/Foto/index.jsx
@@ -1,13 +1,17 @@
-import './style.css'
+import "./style.css";
 
 const Foto = (props) => {
-
-    return (
-        <div className='imgFotoPintura' style={{backgroundImage : `url(${props.linkImagem})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition:"center"}}>
-            <img src={props.linkImagem} alt="" width= "80%" height= "80%"/>
-        </div>
-    )
-}
-
+  return (
+    <div
+      className="imgFotoPintura"
+      style={{
+        backgroundImage: `url(${props.linkImagem})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
+    </div>
+  );
+};
 
 export default Foto;
--- src/pages/PaginaFotos/index.jsx
+++ src/pages/PaginaFotos/index.jsx
@@ -1,6 +1,19 @@
 import Foto from '../../components/Foto';
 import './style.css'
 import imagem012 from "../../display-template/images/012.jpg";
+import imagem013 from "../../display-template/images/013.jpg";
+import imagem014 from "../../display-template/images/014.jpg";
+import imagem017 from "../../display-template/images/017.jpg";
+import imagem018 from "../../display-template/images/018.jpg";
+import imagem019 from "../../display-template/images/019.jpg";
+import imagem020 from "../../display-template/images/020.jpg";
+import imagem021 from "../../display-template/images/021.jpg";
+import imagem022 from "../../display-template/images/022.jpg";
+import imagem023 from "../../display-template/images/023.jpg";
+import imagem024 from "../../display-template/images/024.jpg";
+import imagem025 from "../../display-template/images/025.jpg";
+import imagem026 from "../../display-template/images/026.jpg";
+import imagemFrida from "../../display-template/images/Frida.jpg";
 
 const PaginaFotos = () => {
 
@@ -8,24 +21,22 @@ const PaginaFotos = () => {
         <div className='divDisplay'>
             <div className='div1'>
             <Foto linkImagem={imagem012}></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem={imagem013}></Foto>
+            <Foto linkImagem={imagem014}></Foto>
+            <Foto linkImagem={imagem017}></Foto>
+            <Foto linkImagem={imagem026}></Foto>
+            <Foto linkImagem={imagem018}></Foto>
+            <Foto linkImagem={imagemFrida}></Foto>
             </div>
             <div className='div2'>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
-            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem={imagem019}></Foto>
+            <Foto linkImagem={imagem020}></Foto>
+            <Foto linkImagem={imagem021}></Foto>
+            <Foto linkImagem={imagem022}></Foto>
+            <Foto linkImagem={imagem023}></Foto>
+            <Foto linkImagem={imagem024}></Foto>
+            <Foto linkImagem={imagem025}></Foto>
             </div>
-            
-
         </div>
     )
 }
