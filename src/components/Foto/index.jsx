34ba12982d131740e39a77644c727271639829d9
gitsena <andre.sena@bandtec.com.br>
2022-05-24 09:09:51 -0300

    estilizando display

--- src/components/Foto/index.jsx
+++ src/components/Foto/index.jsx
@@ -4,7 +4,7 @@ const Foto = (props) => {
 
     return (
         <div className='imgFotoPintura' style={{backgroundImage : `url(${props.linkImagem})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition:"center"}}>
-            <img src={props.linkImagem} alt="" />
+            <img src={props.linkImagem} alt="" width= "80%" height= "80%"/>
         </div>
     )
 }
--- src/pages/PaginaFotos/index.jsx
+++ src/pages/PaginaFotos/index.jsx
@@ -1,17 +1,31 @@
 import Foto from '../../components/Foto';
 import './style.css'
+import imagem012 from "../../display-template/images/012.jpg";
 
 const PaginaFotos = () => {
 
     return (
-        <div className='divImagens'>
+        <div className='divDisplay'>
+            <div className='div1'>
+            <Foto linkImagem={imagem012}></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
             <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            </div>
+            <div className='div2'>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            <Foto linkImagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/300px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"></Foto>
+            </div>
             
+
         </div>
     )
 }
